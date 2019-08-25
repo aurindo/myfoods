642b892d652806363bce1ef9b32ddd2d9fa45c54
Aurindo Jorge <aurindo@gmail.com>
2019-08-25 20:14:41 -0300

    Mocking access to rabbitmq

--- orderTaker/src/test/java/com/aurindo/delivery/orderTaker/resource/OrderTakerResourceTest.java
+++ orderTaker/src/test/java/com/aurindo/delivery/orderTaker/resource/OrderTakerResourceTest.java
@@ -3,11 +3,16 @@ package com.aurindo.delivery.orderTaker.resource;
 import com.aurindo.delivery.orderTaker.model.InitialOrder;
 import com.fasterxml.jackson.databind.JsonNode;
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
@@ -31,10 +36,22 @@ public class OrderTakerResourceTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @MockBean
+    private RabbitTemplate rabbitTemplate;
+
+    @MockBean
+    private Queue queue;
+
     private static final Pattern uuidValidation =
             Pattern.compile("([a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8})");
 
     private final String PATH = "/ordertaker/order";
+
+    @Before
+    public void setup() {
+        Mockito.when(queue.getName())
+                .thenReturn("ordertaker");
+    }
 
     @Test
     public void whenPassACorrectOrderShouldReturnNewOrderCode() throws Exception {
