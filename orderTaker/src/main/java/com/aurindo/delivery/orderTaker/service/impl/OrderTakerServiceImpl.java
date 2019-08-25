3943892fb982dae395c1bb0127b284d510f39424
Aurindo Jorge <aurindo@gmail.com>
2019-08-25 11:47:24 -0300

    Adjust tests, send a json as string to rabbitmq

--- orderTaker/src/main/java/com/aurindo/delivery/orderTaker/service/impl/OrderTakerServiceImpl.java
+++ orderTaker/src/main/java/com/aurindo/delivery/orderTaker/service/impl/OrderTakerServiceImpl.java
@@ -3,7 +3,10 @@ package com.aurindo.delivery.orderTaker.service.impl;
 import com.aurindo.delivery.orderTaker.model.InitialOrder;
 import com.aurindo.delivery.orderTaker.service.OrderTakerService;
 import com.aurindo.delivery.orderTaker.service.SenderMessageService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -11,6 +14,7 @@ import java.util.UUID;
 @Service
 public class OrderTakerServiceImpl implements OrderTakerService {
 
+    @Qualifier("senderRabbitMQ")
     @Autowired
     private SenderMessageService senderMessageService;
 
@@ -18,7 +22,11 @@ public class OrderTakerServiceImpl implements OrderTakerService {
     public String processOrder(final InitialOrder initialOrder) {
         initialOrder.setCode(UUID.randomUUID().toString());
 
-        senderMessageService.sendToQueue(initialOrder);
+        try {
+            senderMessageService.sendToQueue(initialOrder);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
 
         return initialOrder.getCode();
     }
--- orderTaker/src/main/java/com/aurindo/delivery/orderTaker/service/impl/SenderMessageRabbitMQ.java
+++ orderTaker/src/main/java/com/aurindo/delivery/orderTaker/service/impl/SenderMessageRabbitMQ.java
@@ -2,11 +2,15 @@ package com.aurindo.delivery.orderTaker.service.impl;
 
 import com.aurindo.delivery.orderTaker.model.InitialOrder;
 import com.aurindo.delivery.orderTaker.service.SenderMessageService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+@Qualifier("senderRabbitMQ")
 @Service
 public class SenderMessageRabbitMQ implements SenderMessageService {
 
@@ -17,8 +21,11 @@ public class SenderMessageRabbitMQ implements SenderMessageService {
     private Queue queue;
 
     @Override
-    public void sendToQueue(final InitialOrder initialOrder) {
-        this.rabbitTemplate.convertAndSend(queue.getName(), initialOrder);
+    public void sendToQueue(final InitialOrder initialOrder) throws JsonProcessingException {
+        ObjectMapper Obj = new ObjectMapper();
+        String jsonInitialOrder = Obj.writeValueAsString(initialOrder);
+
+        this.rabbitTemplate.convertAndSend(queue.getName(), jsonInitialOrder);
     }
 
 }
